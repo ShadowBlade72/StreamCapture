353d91aa2bc029620d435689c1da23555777cc6f
ShadowBlade72 <UnderCoverSPI101@gmail.com>
2023-07-21 18:19:54 -0400

    kick.sh has been integrated into the main script.

--- kick.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-streamer=crazymangovr
-destpath="/Nyx"
-
-if [[ -z $(ps -ef | grep -v grep | grep "https://kick.com/$streamer" | grep streamlink) ]]; then
-	screen -dmS $streamer bash -c "streamlink --output \"$destpath/{author}/{title} {id}.ts\" https://kick.com/$streamer best"
-fi
