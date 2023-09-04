c2c7f7e0148929c9627aabb687a5673d8036d006
ShadowBlade72 <UnderCoverSPI101@gmail.com>
2023-09-04 18:34:07 -0400

    Added game being played to log file

--- streamcapture.sh
+++ streamcapture.sh
@@ -169,7 +169,7 @@ fnStartTwitchRecord(){
 	# Creates an output name of "streamer_S(two digit year)E(julian date)_stream title_[streamid]"
 	outputname=$(echo $request | jq -j --arg jdate $(date +"%j") --arg ydate $(date +"%y") --arg random $RANDOM '.data[].user_login," - S",$ydate,"E",$jdate," - ",.data[].title," [",.data[].id + $random,"]"' | tr -dc '[:print:]' | tr -d '/')
 	if [[ $logging = 1 ]]; then
-		echo -e "[${GREEN}+${NC}] ${BLUE}$(date)${NC} - Starting recording of ${BLUE}$streamer${NC}. File name: ${YELLOW}$outputname.mp4${NC}" | tee -a $destpath/log.txt
+		echo -e "[${GREEN}+${NC}] ${BLUE}$(date)${NC} - Starting recording of ${BLUE}$streamer${NC} playing ${YELLOW}$(echo $request | jq -r '.data[]?.game_name // null')${NC}. File name: ${YELLOW}$outputname.mp4${NC}" | tee -a $destpath/log.txt
 	fi
 	screen -dmS $streamer bash -c "streamlink --stdout https://www.twitch.tv/$streamer best | ffmpeg -i - -c copy \"$destpath/$streamer/$outputname.mp4\""
 }
@@ -178,7 +178,7 @@ fnStartKickRecord(){
 	# Creates an output name of "streamer_S(two digit year)E(julian date)_stream title_[streamid]"
 	outputname=$(echo $request | jq -j --arg jdate $(date +"%j") --arg ydate $(date +"%y") --arg random $RANDOM '.user.username," - S",$ydate,"E",$jdate," - ",.livestream.session_title," [",(.livestream.id|tostring) + $random,"]"' | tr -dc '[:print:]' | tr -d '/')
         if [[ $logging = 1 ]]; then
-		echo -e "[${GREEN}+${NC}] ${BLUE}$(date)${NC} - Starting recording of ${BLUE}$streamer${NC}. File name: ${YELLOW}$outputname.mp4${NC}" | tee -a $destpath/log.txt	
+		echo -e "[${GREEN}+${NC}] ${BLUE}$(date)${NC} - Starting recording of ${BLUE}$streamer${NC} playing ${YELLOW}$(echo $request | jq -r '.livestream.categories[]?.name // null')${NC}. File name: ${YELLOW}$outputname.mp4${NC}" | tee -a $destpath/log.txt	
 	fi
 	screen -dmS $streamer bash -c "streamlink --stdout https://www.kick.com/$streamer best | ffmpeg -i - -movflags faststart -c copy \"$destpath/$streamer/$outputname.mp4\""
 }
